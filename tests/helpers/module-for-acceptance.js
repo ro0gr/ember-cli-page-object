e50127ac483db0ed75b8d6dfd007f92d12d8f46e
Ruslan Grabovoy <kudgo.test@gmail.com>
2017-11-13 23:12:46 +0200

    Rename native-dom to native-events

--- tests/helpers/module-for-acceptance.js
+++ tests/helpers/module-for-acceptance.js
@@ -2,22 +2,22 @@ import { module } from 'qunit';
 import Ember from 'ember';
 import startApp from '../helpers/start-app';
 import destroyApp from '../helpers/destroy-app';
-import { useNativeDOMHelpers } from 'ember-cli-page-object/extend';
+import { useNativeEvents } from 'ember-cli-page-object/extend';
 
 const { RSVP: { Promise } } = Ember;
 
 export default function(name, options = {}) {
-  [false, true].forEach(_useNativeDOMHelpers => {
+  [false, true].forEach(_useNativeEvents => {
     let moduleName = name;
-    if (_useNativeDOMHelpers) {
-      moduleName += ' [native-dom-helpers]';
+    if (_useNativeEvents) {
+      moduleName += ' [native-events]';
     }
 
     module(moduleName, {
       beforeEach() {
         this.application = startApp();
 
-        useNativeDOMHelpers(_useNativeDOMHelpers);
+        useNativeEvents(_useNativeEvents);
 
         if (options.beforeEach) {
           return options.beforeEach.apply(this, arguments);
@@ -25,7 +25,7 @@ export default function(name, options = {}) {
       },
 
       afterEach() {
-        useNativeDOMHelpers(false);
+        useNativeEvents(false);
 
         let afterEach = options.afterEach && options.afterEach.apply(this, arguments);
         return Promise.resolve(afterEach).then(() => destroyApp(this.application));
