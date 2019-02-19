01d38a2185fb26fdbe4f90b58cb10e9d1702fca7
Ruslan Hrabovyi <ruslan.hrabovyi@ligadigital.com>
2019-02-20 01:09:26 +0200

    Switch clickable and click-on-text to use invokeHelper

--- addon-test-support/-private/execution_context/integration.js
+++ addon-test-support/-private/execution_context/integration.js
@@ -33,8 +33,8 @@ IntegrationExecutionContext.prototype = {
 
   visit() {},
 
-  click(selector, container) {
-    this.$(selector, container).click();
+  click(element) {
+    $(element).click();
   },
 
   fillIn(element, content) {
--- addon-test-support/properties/click-on-text/helpers.js
+++ addon-test-support/properties/click-on-text/helpers.js
@@ -1,26 +1,12 @@
-import {
-  assign,
-  buildSelector as originalBuildSelector
-} from '../../-private/helpers';
+import { buildSelector } from '../../-private/helpers';
+import { findElement } from '../../extend';
 
-function childSelector(pageObjectNode, context, selector, options) {
-  // Suppose that we have something like `<form><button>Submit</button></form>`
-  // In this case <form> and <button> elements contains "Submit" text, so, we'll
-  // want to __always__ click on the __last__ element that contains the text.
-  let selectorWithSpace = `${selector || ''} `;
-  let opts = assign({ last: true, multiple: true }, options);
+export function childSelector(pageObject, selector, options) {
+  const selectorWithSpace = `${selector || ''} `;
 
-  if (context.find(selectorWithSpace, opts).length) {
-    return originalBuildSelector(pageObjectNode, selectorWithSpace, opts);
-  }
-}
-
-export function buildSelector(pageObjectNode, context, selector, options) {
-  let childSel = childSelector(pageObjectNode, context, selector, options);
-
-  if (childSel) {
-    return childSel;
+  if (findElement(pageObject, selectorWithSpace, options).length) {
+    return buildSelector(pageObject, selectorWithSpace, options);
   } else {
-    return originalBuildSelector(pageObjectNode, selector, options);
+    return buildSelector(pageObject, selector, options);
   }
 }
