d96e43bfef8d172931c87c32a44447beec766c46
Ruslan Hrabovyi <kudgo.test@gmail.com>
2019-10-28 03:02:04 +0200

    take into account sync errors

--- addon-test-support/extend/action.js
+++ addon-test-support/extend/action.js
@@ -7,11 +7,15 @@ export default function action(node, selector, query, cb) {
     multiple: true
   }, query)).get();
 
-  return domElements.reduce((node, domElement) => {
-    return run(node, () => {
-      return cb(domElement);
+  try {
+    return domElements.reduce((node, domElement) => {
+      return run(node, () => {
+        return cb(domElement);
+      });
+    }, node).then(undefined, (e) => {
+      return throwBetterError(node, query.pageObjectKey, e, { selector })
     });
-  }, node).then(undefined, (e) => {
+  } catch (e) {
     return throwBetterError(node, query.pageObjectKey, e, { selector })
-  });
+  }
 }
